0969bec1b822b29383ecefe5e1446003a4fbdd9a
Caique Magalhaes <130615707+Eike94@users.noreply.github.com>
2025-05-03 23:29:47 -0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,12 @@ const app = express();
 
 // Middlewares principais
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['https://silk-os.vercel.app', 'http://localhost:5173'], // adicione aqui os domínios autorizados
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true
+}));
+
 
 // Rotas
 app.use("/Users", routerUsersAPI);
